aeb65cd551adc77ecce7822528270033d4f8eeb3
Ethelyn Interdictor <einterdi@un-a3.kzn.21-school.ru>
2021-12-08 18:55:08 +0300

    1-st commit

--- pipex.c
+++ pipex.c
@@ -6,47 +6,11 @@
 /*   By: einterdi <einterdi@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 22:05:29 by einterdi          #+#    #+#             */
-/*   Updated: 2021/12/08 17:47:38 by einterdi         ###   ########.fr       */
+/*   Updated: 2021/12/08 18:54:07 by einterdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	if (n == 0)
-		return (0);
-	i = 0;
-	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-	{
-		i++;
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	if (!*needle)
-		return ((char *)haystack);
-	while (i < len && haystack[i])
-	{
-		j = 0;
-		while (haystack[i + j] == needle[j] && i + j < len && needle[j])
-		{
-			if (needle[j + 1] == '\0')
-				return ((char *)&haystack[i]);
-			j++;
-		}
-		i++;
-	}
-	return (NULL);
-}
 
 int main(int argc, char **argv, char **env)
 {
--- pipex.h
+++ pipex.h
@@ -6,13 +6,14 @@
 /*   By: einterdi <einterdi@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 22:05:20 by einterdi          #+#    #+#             */
-/*   Updated: 2021/12/08 18:45:00 by einterdi         ###   ########.fr       */
+/*   Updated: 2021/12/08 18:53:42 by einterdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 #define PIPEX_H
 
+#include "libft/libft.h"
 #include <stdio.h>
 #include <unistd.h>
 
